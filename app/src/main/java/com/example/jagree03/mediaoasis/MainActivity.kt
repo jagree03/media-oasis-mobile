07252009b279297871697ca4771cb75e0054a2b1
jagree03 <jagree03@outlook.com>
2024-10-12 18:27:42 +0100

    Tweaked activity layouts to match added vertical / horizontal huidelines at 16dp margins to ensure the activity's layout can fit properly and ensure the elements aren't misplaced on physical android device screen, added goToRegistration function with intents and connected to the Register button, added showHidePassword function connected to the Show/Hide Password button and any help used for this has been referenced in the report. Started working on validateInformation and checkPasswordStrength methods and plan to work on the next activity.

--- app/src/main/java/com/example/jagree03/mediaoasis/MainActivity.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.jagree03.mediaoasis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToRegistration(view: View) {
+        val send = Intent(this, RegistrationActivity::class.java)
+
+        startActivity(send)
     }
 }
--- app/src/main/java/com/example/jagree03/mediaoasis/RegistrationActivity.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/RegistrationActivity.kt
@@ -1,8 +1,14 @@
 package com.example.jagree03.mediaoasis
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.set
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +22,60 @@ class RegistrationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    /**
+     * This method handles showing and hiding action of the edit text of textPassword type.
+     * The transformation methods used help perform this action
+     *
+     * Reference: https://www.scribd.com/document/716398701/Android-Show-Hide-Password-in-Edittext
+     */
+    fun showHidePassword(view: View) {
+        val editTextPassword: EditText = findViewById(R.id.editTextPasswordInput)
+        val showHidePasswordButton: Button = findViewById(R.id.button_showhidepassword)
+
+        if (showHidePasswordButton.text.equals(getString(R.string.show))) {
+            editTextPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            showHidePasswordButton.setText(getString(R.string.hide))
+        } else if (showHidePasswordButton.text.equals(getString(R.string.hide))) {
+            editTextPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            showHidePasswordButton.setText(getString(R.string.show))
+        }
+    }
+
+    fun validateInformation(view: View) {
+        val firstName: EditText = findViewById(R.id.editTextFirstNameInput)
+        val lastName: EditText = findViewById(R.id.editTextLastNameInput)
+        val email: EditText = findViewById(R.id.editTextEmailInput)
+        val phoneNo: EditText = findViewById(R.id.editTextPhoneNumInput)
+        val username: EditText = findViewById(R.id.editTextUsernameInput)
+        val password: EditText = findViewById(R.id.editTextPasswordInput)
+        var error: String = ""
+
+        if (firstName.text.isEmpty()) {
+            error += "First Name is Empty"
+        } else if (lastName.text.isEmpty()) {
+            error += "Last Name is Empty"
+        } else if (email.text.isEmpty()) {
+            error += "Email Address is Empty"
+        } else if (!email.text.contains('@')) {
+            error += "Email Address missing @ character"
+        } else if (phoneNo.text.isEmpty()) {
+            error += "Phone Number is Empty"
+        } else if (username.text.isEmpty()) {
+            error += "Username is Empty"
+        } else if (password.text.isEmpty()) {
+            error += "Password is Empty"
+        }
+
+        error += validatePasswordStrength(password.text.toString())
+
+        //return error
+    }
+
+    fun validatePasswordStrength(password: String): String {
+        var error: String = ""
+
+        return error
     }
 }
