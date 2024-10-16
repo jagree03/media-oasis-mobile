f9466b63d7aa1882dfd4761798a10a937f2cbc8b
jagree03 <jagree03@outlook.com>
2024-10-17 00:26:18 +0100

    Added registration error notifications via Toast

--- app/src/main/java/com/example/jagree03/mediaoasis/RegistrationActivity.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/RegistrationActivity.kt
@@ -7,6 +7,7 @@ import android.text.method.PasswordTransformationMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.set
@@ -71,6 +72,7 @@ class RegistrationActivity : AppCompatActivity() {
 
         error += validatePasswordStrength(password.text.toString())
 
+        Toast.makeText(applicationContext, error, Toast.LENGTH_SHORT).show()
         //return error
     }
 
