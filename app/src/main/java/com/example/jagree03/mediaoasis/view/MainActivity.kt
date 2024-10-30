faa7d44e62c9ceb2d551be87eef9a3f1afdcf955
jagree03 <jagree03@outlook.com>
2024-10-30 23:46:21 +0000

    Started working on a basic login system which utilises the DataBaseHelper methods, also includes a basic bruteforce prevention system via login attempts which still needs work, I have associated the onClick attribute of the Sign In button of the main login activity to the login method in the MainActivity.kt file, essentially making the Sign In button clickable.

--- app/src/main/java/com/example/jagree03/mediaoasis/view/MainActivity.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/view/MainActivity.kt
@@ -4,9 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.example.jagree03.mediaoasis.model.DataBaseHelper
+import com.example.jagree03.mediaoasis.model.User
 
 class MainActivity : AppCompatActivity() {
+
+    var loginAttempts: Int = 3
+    var loginDisabled: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,8 +29,61 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    fun login(view: View) {
+
+        val message: String?
+        val userNameEditText: EditText = findViewById(R.id.editTextUsernameInput)
+        val userName: String = userNameEditText.text.toString()
+        val userPasswordEditText: EditText = findViewById(R.id.editTextPasswordInput)
+        val userPassword: String = userPasswordEditText.text.toString()
+
+        if (loginAttempts == 0) {
+            displayMessageAlert("Login disabled")
+            loginDisabled = true
+        }
+        else
+            loginAttempts -= 1 // subtract 1 from login attempts (default is 3)
+
+        while (loginDisabled == true) {
+            userNameEditText.isVisible = false
+            userPasswordEditText.isVisible = false
+        }
+
+        if (userName.isEmpty() || userPassword.isEmpty()) // if empty (null), display a notification to the user
+            Toast.makeText(this,"Please enter your username and password to sign in.",Toast.LENGTH_LONG).show()
+        else {
+            var loginConfirmed: Boolean = false
+            val myDataBase = DataBaseHelper(this)
+            val result = myDataBase.getUser(
+                User(-1," ", " ",
+                " ", " ", userName, userPassword, 0))
+
+            println("Database =  $result")
+
+            if (result == -1)
+                message = "User not found, please try again."
+            else if (result == -2)
+                message = "Error: cannot open or create the database"
+            else { // any other result means the user was found in the database
+                message = "Welcome, $userName"
+                loginConfirmed = true
+            }
+
+            displayMessageAlert(message)
+
+            if (loginConfirmed)
+                goToMainMenu()
+        }
+    }
+
     fun goToRegistration(view: View) {
         val send = Intent(this, RegistrationActivity::class.java)
+
+        startActivity(send)
+    }
+
+    fun goToMainMenu() {
+        val send = Intent(this, MenuActivity::class.java)
 
         startActivity(send)
     }
@@ -29,5 +92,12 @@ class MainActivity : AppCompatActivity() {
         val send = Intent(this, MenuActivity::class.java)
 
         startActivity(send)
+    }
+
+    fun displayMessageAlert(message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(message)
+        builder.create()
+        builder.show()
     }
 }
