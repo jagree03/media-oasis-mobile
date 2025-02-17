fa5ee0c9afeff997369e9f82e4ec0129a28e79ad
jagree03 <jagree03@outlook.com>
2025-02-17 23:05:02 +0000

    Updated debug menu item to represent Manage Registered Accounts Admin Menu Fragment as well as the fragment transaction operation within MenuActivity.kt, this has been done in order to allow this fragment to be tested.

--- app/src/main/java/com/example/jagree03/mediaoasis/view/MenuActivity.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/view/MenuActivity.kt
@@ -126,14 +126,14 @@ class MenuActivity : AppCompatActivity() {
                 }
 
                 // DEBUG
-                R.id.debugAddSale -> {
+                R.id.debugManageRegAccounts -> {
 
-                    Toast.makeText(applicationContext, "Entering debug add new sale item fragment...", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Entering debug manage reg accounts...", Toast.LENGTH_SHORT).show()
 
-                    val addSaleFragment = Admin_AddNewSaleItem()
+                    val manageRegAccounts = Admin_ManageRegisteredAccountsFragment()
 
                     supportFragmentManager.beginTransaction().apply { // begin fragment transaction operation
-                        replace(R.id.frameLayout, addSaleFragment) // replacing the fragment in the frameLayout container of the activity with the shopping cart fragment
+                        replace(R.id.frameLayout, manageRegAccounts) // replacing the fragment in the frameLayout container of the activity with the shopping cart fragment
                         addToBackStack(null) // allows you to go back with android back arrow navigation key
                         commit() // applying the replacement operation and commiting the transaction
                     }
