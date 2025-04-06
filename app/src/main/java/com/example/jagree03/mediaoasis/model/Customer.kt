9f51cfa104ed035c477c53dd0775180e1e4ea34a
jagree03 <jagree03@outlook.com>
2025-04-06 17:45:39 +0100

    Modified User.kt data class in model to Customer.kt and also removed isAdmin attribute for security and scalability purposes, It's better to segregate Customer and Admin as two separate classes.

---
+++ app/src/main/java/com/example/jagree03/mediaoasis/model/Customer.kt
@@ -0,0 +1,11 @@
+package com.example.jagree03.mediaoasis.model
+
+data class Customer(
+    var custID: Int,
+    var custFirstName: String,
+    var custLastName: String,
+    var custEmail: String,
+    var custPhoneNo: String,
+    var custUsername: String,
+    var custPassword: String) {
+}
