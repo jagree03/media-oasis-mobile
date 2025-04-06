c10061f445811ee25d6fb69dab1de87447b1eb7e
jagree03 <jagree03@outlook.com>
2025-04-07 00:29:10 +0100

    Updated Product.kt data class attributes names to reflect camel-casing for consistency and readability, also moved prodCategory above prodIsAvailable attribute and added a null-safety check to the prodDescription variable as it is not technically required to have a description for an item, therefore it can be null and is a nullable reference.

--- app/src/main/java/com/example/jagree03/mediaoasis/model/Product.kt
+++ app/src/main/java/com/example/jagree03/mediaoasis/model/Product.kt
@@ -2,6 +2,11 @@ package com.example.jagree03.mediaoasis.Model
 import java.io.Serializable
 
 data class Product(
-    val ProdId: Int, var ProdName: String, var ProdDescription: String, var ProdPrice: Double,
-	var ProdImage: ByteArray?, var ProdAvailable: Int, var ProdCategory: String) : Serializable {
+    val prodId: Int,
+    var prodName: String,
+    var prodDescription: String?,
+    var prodPrice: Double,
+	var prodImage: ByteArray?,
+    var prodCategory: String,
+    var prodIsAvailable: Boolean) : Serializable {
 }
